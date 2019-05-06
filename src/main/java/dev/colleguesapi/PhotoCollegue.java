5d1a79d42c8b2d08f22b2873580e4b27de5720e0
Taya-Huas-Ca <tayahuasca@caramail.com>
2019-05-06 22:14:52 +0200

    ajout des getters/setters pour afficher la galerie côté front

--- src/main/java/dev/colleguesapi/PhotoCollegue.java
+++ src/main/java/dev/colleguesapi/PhotoCollegue.java
@@ -3,9 +3,25 @@ package dev.colleguesapi;
 public class PhotoCollegue {
 	String matricule;
 	String photoUrl;
-	
+
 	public PhotoCollegue(String matricule, String photoUrl) {
 		this.matricule = matricule;
+		this.photoUrl = photoUrl;
+	}
+	
+	public String getMatricule() {
+		return matricule;
+	}
+	
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+	
+	public void setPhotoUrl(String photoUrl) {
 		this.photoUrl = photoUrl;
 	}
 }
--- src/main/java/dev/colleguesapi/StartupDataInit.java
+++ src/main/java/dev/colleguesapi/StartupDataInit.java
@@ -30,7 +30,7 @@ public class StartupDataInit {
 		c1.setEmail(generateEmail(c1));
 
 		Collegue c2 = new Collegue(UUID.randomUUID().toString(), "Bricot", "Judas", email, LocalDate.of(2000, 3, 30),
-				"https://www.chiens-de-france.com/photo/eleveurs/255/50814/album/a8f6ec0f-fa0f-24d4-b1a8-9086d22b2dad_min.jpg");
+				"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.1rwq_803lgbZQQq4QwZ7pwHaHa%26pid%3DApi&f=1");
 		c2.setEmail(generateEmail(c2));
 
 		Collegue c3 = new Collegue(UUID.randomUUID().toString(), "Dépot", "Bricot", email, LocalDate.of(2000, 2, 29),
@@ -38,7 +38,7 @@ public class StartupDataInit {
 		c3.setEmail(generateEmail(c3));
 
 		Collegue c4 = new Collegue(UUID.randomUUID().toString(), "Fruité", "Saumon, Mustélipon", email,
-				LocalDate.of(1993, 3, 20), photoUrl);
+				LocalDate.of(1993, 3, 20), "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/John_William_Waterhouse_-_Mermaid.JPG/220px-John_William_Waterhouse_-_Mermaid.JPG");
 		c4.setEmail(generateEmail(c4));
 
 		Collegue c5 = new Collegue(UUID.randomUUID().toString(), "Huasca", "Taya", email, LocalDate.of(1997, 6, 24),
@@ -46,14 +46,21 @@ public class StartupDataInit {
 		c5.setEmail(generateEmail(c5));
 
 		Collegue c6 = new Collegue(UUID.randomUUID().toString(), "Huasca", "Tiny", email, LocalDate.of(1997, 6, 24),
-				photoUrl);
+				"https://www.chiens-de-france.com/photo/eleveurs/255/50814/album/a8f6ec0f-fa0f-24d4-b1a8-9086d22b2dad_min.jpg");
 		c6.setEmail(generateEmail(c6));
 
-		Collegue c7 = new Collegue(UUID.randomUUID().toString(), "Fonfec", "Sophie", email, LocalDate.of(1993, 03, 20),
-				"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fdhs.gov%2Fsites%2Fdefault%2Ffiles%2Fimages%2FREAL-ID_icon_public.png&f=1");
+		Collegue c7 = new Collegue(UUID.randomUUID().toString(), "Fonfec", "Sophie", email, LocalDate.of(1998, 07, 12),
+				"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.meatsandsausages.com%2Fpublic%2Fimages%2Fsausage-recipes%2Fsaucisson-beaufort.jpg&f=1");
 		c7.setEmail(generateEmail(c7));
 	
-		collegueRepo.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7));
+		Collegue c8 = new Collegue(UUID.randomUUID().toString(), "Landes", "Yves", email, LocalDate.of(1998, 12, 20),
+				"https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fcigalemistralavande.c.i.pic.centerblog.net%2Fo7szr9qf.jpg&f=1");
+		c8.setEmail(generateEmail(c8));
+		
+		Collegue c9 = new Collegue(UUID.randomUUID().toString(), "J'ai su", "Jean, Luc, Mathieu, Marc", email, LocalDate.of(0001, 12, 24), photoUrl);
+		c9.setEmail(generateEmail(c9));
+		
+		collegueRepo.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9));
 
     }
     
